f8a6b469c08b6598dd0cff302ec3bb3829bb3e4a
bobowang2017 <409879654@qq.com>
2019-06-02 15:33:19 +0800

    commit

--- apis/latest_used/views.py
+++ apis/latest_used/views.py
@@ -48,4 +48,6 @@ class ApplicationVIew(Resource):
         logger.info("*"*60)
         data = Application.query.all()
         result = app_schema.dump(data, many=True)
+
+        hello = db.session.query(Application).join(Application.project).all()
         return {"success": result.data}
