bdf4c4c674814d0eee74cc9bca443565fbd25166
bobowang2017 <409879654@qq.com>
2019-06-22 19:35:25 +0800

    提交代码

--- apis/student/views.py
+++ apis/student/views.py
@@ -18,8 +18,12 @@ class StudentView(Resource):
         # 这种方式返回的只有Teacher.name, Student.name字段
         # data = db.session.query(Teacher.name, Student.name).join(Teacher, Student.teacher_id == Teacher.id).all()
         # 这种方式返回的是指定的字段，不过是列表元组的形式返回
-        data = db.session.query(Teacher, Student).join(Teacher, Student.teacher_id == Teacher.id).with_entities(
-            Teacher.name, Teacher.sex, Student.id, Student.name, Student.sex, Student.birthday).all()
+
+        query = db.session.query(Teacher, Student).join(Teacher, Student.teacher_id == Teacher.id).with_entities(
+            Teacher.name, Teacher.sex, Student.id, Student.name, Student.sex, Student.birthday)
+        data = query.all()
+        desc = query.column_descriptions
+        print(desc)
         res = [dict(zip(('tname', 'tsex', 'sid', 'sname', 'ssex', 'sbir'), _d)) for _d in data]
         return jsonify({'code': 0, 'msg': res})
         # return json.dumps({'code': 0, 'msg': res}, ensure_ascii=False)
